dfc6584f5124ff98772c503ff9ca9a02f6179718
Marcel Mantovani Filho <37183770+marcelmantovani@users.noreply.github.com>
2020-10-14 12:24:12 -0400

    spell error fix.

--- src/numbers/WaysToPaintGrid.java
+++ src/numbers/WaysToPaintGrid.java
@@ -18,7 +18,7 @@ public class WaysToPaintGrid {
 	public int Solution (int n) {
 		int MOD = (int) (1e9 + 7);
 		/*
-		init: for the single row, there are C(3, 3) * 3! = 6 3-color combinations, and C(3, 2) * 2 = 6 2-color combinations.
+		Init: for the single row, there are C(3, 3) * 3! = 6 3-color combinations, and C(3, 2) * 2 = 6 2-color combinations.
 		C(3, 3) * 3! means: choose 3 colors out of 3, and, first cell: 3 possibilities, second cell: 2 possibilities, third cell: only 1 possibility.
 		C(3, 2) * 2 means: choose 2 colors out of 3, and, there are only two ways: ABA or BAB.
 		*/
